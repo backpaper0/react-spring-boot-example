63f750096878fdb7488d4177c5d11cb4660579d1
Uragami Taichi <backpaper0@gmail.com>
2020-03-15 16:58:12 +0900

    Nginxのtry_files相当の処理

---
+++ example-api/src/main/java/com/example/config/IndexHtmlConfig.java
@@ -0,0 +1,49 @@
+package com.example.config;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class IndexHtmlConfig implements WebMvcConfigurer {
+
+    private final ResourceProperties resourceProperties;
+
+    public IndexHtmlConfig(final ResourceProperties resourceProperties) {
+        this.resourceProperties = resourceProperties;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(getSeconds(resourceProperties.getCache().getPeriod()))
+                .setCacheControl(
+                        resourceProperties.getCache().getCachecontrol().toHttpCacheControl())
+                .resourceChain(true)
+                .addResolver(new IndexHtmlResourceResolver());
+    }
+
+    private static Integer getSeconds(final Duration cachePeriod) {
+        return (cachePeriod != null) ? (int) cachePeriod.getSeconds() : null;
+    }
+
+    private static class IndexHtmlResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource getResource(final String resourcePath,
+                final Resource location)
+                throws IOException {
+            final Resource resource = super.getResource(resourcePath, location);
+            if (resource != null) {
+                return resource;
+            }
+            return super.getResource("index.html", location);
+        }
+    }
+}
